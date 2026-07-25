4f1d21a34112c921bbed95aa643c060b9a54b42f
mae134 <en.tps.luc@gmail.com>
2026-07-25 23:05:52 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/mae134/equipmentinspection/EquipmentInspectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mae134.equipmentinspection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentInspectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentInspectionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mae134/equipmentinspection/EquipmentInspectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mae134.equipmentinspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentInspectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
