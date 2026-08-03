604fa474af712d2973ed368c9f8c951be662afb6
mae134 <en.tps.luc@gmail.com>
2026-08-03 13:25:16 +0900

    feat: add Equipment repository

---
+++ src/main/java/com/mae134/equipmentinspection/equipment/EquipmentRepository.java
@@ -0,0 +1,5 @@
+package com.mae134.equipmentinspection.equipment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {}
