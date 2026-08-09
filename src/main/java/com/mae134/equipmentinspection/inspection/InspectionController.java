65ff99aa409fa246dc5b9d2bde9f77530058d273
mae134 <en.tps.luc@gmail.com>
2026-08-09 23:46:42 +0900

    fix: return 204 for inspection deletion

--- src/main/java/com/mae134/equipmentinspection/inspection/InspectionController.java
+++ src/main/java/com/mae134/equipmentinspection/inspection/InspectionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -46,6 +47,7 @@ public class InspectionController {
   }
 
   @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable Long id) {
     inspectionService.delete(id);
   }
